68fd6a3707802c26bc03016517ea2e93cee8c26e
Beatriz Dias <beatrizdias.ds@outlook.com>
2026-05-05 11:40:35 -0300

    removendo pasta antiga

--- exercicios python iniciante/conversor_temperatura.py
+++
@@ -1,5 +0,0 @@
-celsius = float(input("Digite a temperatura em graus Celsius: "))
-
-fahrenheit = (celsius * 9/5) + 32
-
-print("Temperatura em Fahrenheit:", fahrenheit)
--- exercicios python iniciante/troca_valores.py
+++
@@ -1,9 +0,0 @@
-A = int(input("Digite o valor de A: "))
-B = int(input("Digite o valor de B: "))
-
-temp = A
-A = B
-B = temp
-
-print("Novo valor de A:", A)
-print("Novo valor de B:", B)
